d32ba1e00fb427300ee102d2b3aac51a1d7a17d5
saheel <sahilsheakh86@gmial.com>
2026-07-04 02:59:40 +0300

    Initial commit

--- src/data/story.js
+++ src/data/story.js
@@ -1,7 +1,7 @@
 import img1 from "../assets/images/one.JPG";
 import img2 from "../assets/images/day.JPG";
 import img3 from "../assets/images/firstpic.JPG";
-import img4 from "../assets/images/funday.PNG";
+import img4 from "../assets/images/funday.jpg";
 import img5 from "../assets/images/img05.PNG";
 
 export const story = [
